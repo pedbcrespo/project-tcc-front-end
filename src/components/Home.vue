474b644f58dd4229d8c1ae1625a9264cf3e46db0
pedro <pedbcrespo@gmail.com>
2023-09-18 09:01:46 -0300

    instalando bibliotecas e criando componente home

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    oi
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
