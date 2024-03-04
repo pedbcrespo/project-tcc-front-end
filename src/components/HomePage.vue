d1c5a5c9fa9c4457e17c67d0c3c245d61e6f21b9
pedro <pedbcrespo@gmail.com>
2024-03-04 09:07:39 -0300

    fazendo juncai inicial com o backend

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,7 +3,7 @@
     <title-header />
     <div v-if="isFormVisible">
       <form>
-        
+
       </form>
     </div>
   </div>
@@ -12,20 +12,54 @@
 <script>
 import TitleHeader from './TitleHeader.vue';
 import BackendService from '@/service/backendService';
-import Question from '@/model/Questions';
-import Question from './Question.vue';
+// import StructQuestion from '@/model/StructQuestion';
+// import FormQuestion from './FormQuestion.vue';
 
 export default {
   name: 'home-page',
-  components: { TitleHeader, Question },
-  mounted(){
-    console.log('INICIANDO');
-  },
+  components: { TitleHeader },
   data() {
     return {
       isFormVisible: true,
       service: new BackendService(),
       questions: [],
+      cities: [],
+    }
+  },
+  mounted() {
+    console.log('INICIANDO');
+    this.service.getCities('RJ').then(res => {
+      this.cities = res.data;
+      console.log(this.cities);
+    });
+    this.getQuestions();
+  },
+  methods: {
+    requestQuestions(callback) {
+      const pathJsonFile = 'src/static/text_questions.json'     
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          const json = JSON.parse(xhr.responseText);
+          callback(null, json);
+        } else if (xhr.readyState === 4) {
+          callback(new Error('Erro ao ler o arquivo JSON'));
+        }
+      },
+        xhr.open('GET', pathJsonFile, true);
+      xhr.responseType = 'text';
+      xhr.send();
+    },
+    getQuestions() {
+      const callback = function(err, data) {
+        if(err)
+          console.error(err);
+        else{
+          console.log(data)
+          return data;
+        }
+      } 
+      this.requestQuestions(callback);
     }
   }
 }
@@ -49,4 +83,4 @@ li {
 a {
   color: #42b983;
 }
-</style>@/static/textQuestions
+</style>@/static/textQuestions@/model/Question./FormQuestion.vue
