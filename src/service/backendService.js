3e8e71003478652e9c5db02dd407a8f58e268979
pedro <pedbcrespo@gmail.com>
2024-03-05 09:08:36 -0300

    adicionando request de recomendacao

--- src/service/backendService.js
+++ src/service/backendService.js
@@ -4,7 +4,12 @@ export default class BackendService {
     constructor() {
         this.url = 'http://127.0.0.1:5000/info-api'
     }
+    
     getCities(uf) {
         return axios.get(`${this.url}/state/${uf}`)
+    }
+
+    getRecomendation(formAnswers) {
+        return axios.post(`${this.url}/city/recomendation`, formAnswers)
     }
 }
