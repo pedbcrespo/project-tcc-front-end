bd28588fb5fb71ca95311274b5b4b80166cbf2ca
pedro <pedbcrespo@gmail.com>
2024-03-10 11:57:02 -0300

    ajustando model

--- src/model/AnsweredQuestion.js
+++ src/model/AnsweredQuestion.js
@@ -1,6 +1,7 @@
 export default class Question {
-    constructor(title) {
-        this.title = title;
+    constructor(question) {
+        this.title = question.title;
+        this.attribute = question.attribute;
         this.alternatives = [1, 2, 3, 4, 5]
         this.answer = null;
     }
