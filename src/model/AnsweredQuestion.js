4bbe0c617428946e10f7cf01d6fc9766769e24d7
pedro <pedbcrespo@gmail.com>
2024-03-24 19:14:40 -0300

    ajustando modelagem de perguntas

--- src/model/AnsweredQuestion.js
+++ src/model/AnsweredQuestion.js
@@ -1,9 +1,16 @@
 export default class Question {
     constructor(question) {
         this.title = question.title;
-        this.increase = question.increase; 
-        this.decrease = question.decrease; 
+        this.increase = question.increase;
+        this.decrease = question.decrease;
         this.alternatives = [1, 2, 3, 4, 5]
         this.answer = null;
+        this.hoursLightEstiamte = 0;
+        this.ltWaterConsume = 0;
+        this.alimentation = 0;
+        this.hygiene = 0;
+        this.transportation = 0;
+        this.health = 0;
+        this.recreation = 0;
     }
 }
